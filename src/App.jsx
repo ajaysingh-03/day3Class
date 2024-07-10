c6fdb0426aa217b90bc2a866345a5585ae825594
AjaySingh <ajayingh03@gmail.com>
2024-07-11 04:44:29 +0530

    added Quotes page

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,26 @@ import Hero from '../Components/Hero/hero';
 import Card from '../Components/Card/card';
 import Grid from '../Components/Grid/grid,';
 import Footer from '../Components/Footer/footer';
-function App() {
+import Quote from '../Components/Quote/quote';
+import Home from '../Components/Home/home';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from '../Components/Home/home';
+
+function App() {
   return (
     <>
-      <Header/>
-      <Hero/>
-      <Card/>
-      <Grid/>
-      <Footer/>
+      <Router>
+          <Header/>
+          {/* <Hero /> */}
+          <Routes>
+            <Route path="/" element={<Home/>} /> 
+            {/* <Route path="/quote" element={<Quote/>} /> */}
+            <Route path="/quote" element={<Quote />} />
+
+          </Routes>
+          <Footer/>
+      </Router>
     </>
   )
 }
